78559b8107b90a6411ca653d6ed5779a4bc58521
Jay <Jay@DESKTOP-JGRTLDJ>
2023-08-12 21:31:38 +0530

    Assignment First Code push

---
+++ Java_Assignments/src/main/java/Assignments/Java_Assignments/App.java
@@ -0,0 +1,13 @@
+package Assignments.Java_Assignments;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
